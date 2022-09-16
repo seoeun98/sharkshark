9ec5bf98d192e88bbeee557da577111304f0d50a
seoeun98 <vcv0174@naver.com>
2022-09-16 10:23:48 +0900

    fastapi 기본

---
+++ back/pythonProject/sql_app/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://ssafy:ssafy@localhost:3306/b205"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()

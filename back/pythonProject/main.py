e84e39eb6d2c9ab59b864b5466144936d8ca1473
seoeun98 <vcv0174@naver.com>
2022-09-21 14:17:21 +0900

    CORS

--- back/pythonProject/main.py
+++ back/pythonProject/main.py
@@ -1,12 +1,26 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from sql_app import models
 from sql_app.database import engine
-from sql_app.routers import users, rivals
+from sql_app.routers import users, rivals, problems
 
 app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 models.Base.metadata.create_all(engine)
 
 app.include_router(users.router)
 app.include_router(rivals.router)
+app.include_router(problems.router)

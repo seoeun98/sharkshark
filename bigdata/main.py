5f8b6da4fb3858ae253f486356ae7b6d1b62cb88
최진호 <84559471+turtlebooster@users.noreply.github.com>
2022-09-27 14:32:24 +0900

    Feat : add csv files into db function
    Former-commit-id: 33d97f63afb620a5064d2ecc8a1225aa0c00121a

--- bigdata/main.py
+++ bigdata/main.py
@@ -4,34 +4,68 @@ import pandas as pd
 
 from recommend.users import recommend_user_by_tag_knn
 
+# DB 테이블 생성
 models.Base.metadata.create_all(engine)
 
-columns = ['userId', 'math', 'implementation', 'greedy', 'string', 'dataStructure', 'graph', 'dp', 'bruteforce', 'no']
+# csv 데이터 폴더 위치
+file_path = 'data/'
 
-df = recommend_user_by_tag_knn.df
-df['no'] = range(1, len(df) + 1)
-df.columns = columns
-df = pd.DataFrame(df, columns=columns)
-# df['no'] = df['no'] + 1
-# df = df.head(1)
-df = df.drop_duplicates(['userId'])
+# major_category 테이블 입력
+def data_into_major_category():	
+	# major_category 에 사용될 columns
+	columns = ['userId', 'math', 'implementation', 'greedy', 'string', 'dataStructure', 'graph', 'dp', 'bruteforce', 'no']
+
+	# csv 파일 읽어오기 (데이터 프레임으로)
+	df = pd.read_csv(file_path + 'user_avg_level_of_tags_.csv', index_col=0)
+
+	# column 명 db에 맞게 변환
+	df.columns = columns
+	# df = pd.DataFrame(df, columns=columns)
+
+	# userId 중복 제거
+	df = df.drop_duplicates(['userId'])
+
+	# no column 추가
+	df['no'] = range(1, len(df) + 1)
+
+	# db에 데이터 입력
+	df.to_sql(name='major_category', con=engine, if_exists='append', index=False)
+
+# bj_user 테이블 입력
+def data_into_bj_user():	
+	# bj_user 에 사용될 columns	
+	bj_users_columns = ['userId', 'solvedCount', 'userClass', 'tier', 'rating', 'ratingByProblemsSum', 'ratingByClass', 'ratingBySolvedCount', 'exp', 'rivalCount', 'reverseRivalCount', 'maxStreak', 'rank', 'organization']
+	solved_problems_columns = ['userId', 'problems']
+
+	# csv 파일 읽어오기 (데이터 프레임으로)
+	df_bj_users = pd.read_csv(file_path + 'users.csv', index_col=0)
+	df_solved_problems = pd.read_csv(file_path + 'user_solved_problems.csv')
+
+	# column 명 db에 맞게 변환
+	df_bj_users.columns = bj_users_columns	
+	df_solved_problems.columns = solved_problems_columns
 
 
-# conn = engine.connect()
+	# userId 중복 제거
+	df_bj_users = df_bj_users.drop_duplicates(['userId'])
+	df_solved_problems = df_solved_problems.drop_duplicates(['userId'])
 
-# df = pd.DataFrame({ 'no' : [1],
-# 					'userId' : ['turtlebooster'],
-# 					'math' : [0.0],
-# 					'implementation' : [0.0],
-# 					'greedy': [0.0],
-# 					'string': [0.0],
-# 					'dataStructure': [0.0],
-# 					'graph' : [0.0],
-# 					'dp' : [0.0],
-# 					'bruteforce' : [0.0]})
+	# 데이터프레임 합치기
+	df_bj_users = pd.merge(df_bj_users, df_solved_problems, how='inner',on='userId')
 
-print(df)
+	# no column 추가
+	df_bj_users['no'] = range(1, len(df_bj_users) + 1)
 
-df.to_sql(name='major_category', con=engine, if_exists='append', index=False)
 
-# conn.close()
+	# db에 데이터 입력
+	df_bj_users.to_sql(name='bj_user', con=engine, if_exists='append', index=False)
+
+
+# db 연결
+conn = engine.connect()
+
+# major_category 테이블 입력
+# data_into_major_category()
+data_into_bj_user()
+
+conn.close()

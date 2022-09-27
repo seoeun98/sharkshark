0a007f2ad8fb8a4595bbd4cd18b3e7fa9126ab23
최진호 <84559471+turtlebooster@users.noreply.github.com>
2022-09-27 14:32:24 +0900

    Feat : add csv files into db function
    Former-commit-id: 2dfd3089af09d0b1f81b4b4e68abb17aebe79524 [formerly 5f8b6da4fb3858ae253f486356ae7b6d1b62cb88] [formerly 5f8b6da4fb3858ae253f486356ae7b6d1b62cb88 [formerly 33d97f63afb620a5064d2ecc8a1225aa0c00121a]]
    Former-commit-id: f169a4a248678d1b75e981782dd0ebe3cd7d011f [formerly f510aec028e70487a3f4c7e5684a12ba4bdb03b2]
    
    Former-commit-id: 999864074effd852aef23199ea917cc925100873

--- bigdata/sql_app/models.py
+++ bigdata/sql_app/models.py
@@ -4,20 +4,20 @@ from sqlalchemy.orm import relationship
 from .database import Base
 
 
-# class User(Base):
-#     __tablename__ = "user"
+class User(Base):
+    __tablename__ = "user"
 
-#     no = Column(Integer, primary_key=True, index=True, autoincrement=True)
-#     # id = Column(VARCHAR(45), ForeignKey('BJ_user.userId'))
-#     # BJ 통해서 인증한 후에 회원가입 되는 거라면 왜 굳이 FK로 이어야하지? 어차피 id가 같으니 연동돼있지 않아도 상관없는 거 아닌가
+    no = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    # id = Column(VARCHAR(45), ForeignKey('BJ_user.userId'))
+    # BJ 통해서 인증한 후에 회원가입 되는 거라면 왜 굳이 FK로 이어야하지? 어차피 id가 같으니 연동돼있지 않아도 상관없는 거 아닌가
 
-#     id = Column(VARCHAR(45), unique=True)
-#     pw = Column(VARCHAR(120))
-#     token = Column(VARCHAR(45))
-#     git = Column(VARCHAR(45))
-#     dir = Column(VARCHAR(45))
+    id = Column(VARCHAR(45), unique=True)
+    pw = Column(VARCHAR(120))
+    token = Column(VARCHAR(45))
+    git = Column(VARCHAR(45))
+    dir = Column(VARCHAR(45))
 
-#     rival = relationship("rival", backref="users", uselist=False, cascade="all,delete")
+    rival = relationship("rival", backref="users", uselist=False, cascade="all,delete")
 
 # class userMsg(Base):
 #     __tablename__ = "userMsg"
@@ -45,13 +45,11 @@ class major_category(Base):
 class BJ_user(Base):
     __tablename__ = "BJ_user"
 
-    no = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    tier = Column(VARCHAR(45))
-    totalSolvedCnt = Column(Integer)
+    no = Column(Integer, primary_key=True, index=True, autoincrement=True)    
     userId = Column(VARCHAR(45), unique=True)
-
     solvedCount = Column(Integer)
     userClass = Column(Integer)
+    tier = Column(VARCHAR(45))    
     rating = Column(Integer)
     ratingByProblemsSum = Column(Integer)
     ratingByClass = Column(Integer)
@@ -61,7 +59,7 @@ class BJ_user(Base):
     reverseRivalCount = Column(Integer)
     maxStreak = Column(Integer)
     rank = Column(Integer)
-    organization = Column(Integer)
+    organization = Column(VARCHAR(200))
     problems = Column(TEXT)
 
     # user = relationship("user", backref="BJ_users", uselist=False, cascade="all,delete")
@@ -69,12 +67,12 @@ class BJ_user(Base):
     solvedProblem = relationship("solvedProblem", backref="BJ_users", cascade="all,delete")
     major_category = relationship("major_category", backref="BJ_users", cascade="all,delete")
 
-# class rival(Base):
-#     __tablename__ = "rival"
+class rival(Base):
+    __tablename__ = "rival"
 
-#     no = Column(Integer, primary_key=True, index=True, autoincrement=True)
-#     userId = Column(VARCHAR(45), ForeignKey('user.id'))
-#     rivalId = Column(VARCHAR(45), ForeignKey('BJ_user.userId'))
+    no = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    userId = Column(VARCHAR(45), ForeignKey('user.id'))
+    rivalId = Column(VARCHAR(45), ForeignKey('BJ_user.userId'))
 
 class solvedProblem(Base):
     __tablename__ = "solvedProblem"
@@ -98,20 +96,20 @@ class problem(Base):
     problem = relationship("solvedProblem", cascade="all,delete")
     problem_tag = relationship("problem_tag", cascade="all,delete")
 
-class problem_tag(Base):
-    __tablename__ = "problem_tag"
+# class problem_tag(Base):
+#     __tablename__ = "problem_tag"
 
-    no = Column(Integer, primary_key=True, autoincrement=True)
-    probNo = Column(Integer, ForeignKey('problem.no'))
-    tagNo = Column(Integer, ForeignKey('tag.no'))
+#     no = Column(Integer, primary_key=True, autoincrement=True)
+#     probNo = Column(Integer, ForeignKey('problem.no'))
+#     tagNo = Column(Integer, ForeignKey('tag.no'))
 
-class tag(Base):
-    __tablename__ = "tag"
+# class tag(Base):
+#     __tablename__ = "tag"
 
-    no = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(VARCHAR(45))
+#     no = Column(Integer, primary_key=True, autoincrement=True)
+#     name = Column(VARCHAR(45))
 
-    problem_tag = relationship("problem_tag", cascade="all,delete")
+#     problem_tag = relationship("problem_tag", cascade="all,delete")
 
 class wrongType(Base):
     __tablename__ = "wrongType"

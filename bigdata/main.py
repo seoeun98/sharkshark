8ef2aebd378936341e85adc0d93ed277d856f52a
최진호 <ho6778@naver.com>
2022-09-28 14:41:29 +0900

    Feat : add worngtype date into db
    Former-commit-id: ca2a7242883bc8f5fa377a63dde359973a5ec53c [formerly ca2a7242883bc8f5fa377a63dde359973a5ec53c [formerly 1167e1fa72b42f58fceb9e7377a480fd4b75d612]]
    Former-commit-id: a0e8d21bd7af7ac048aee6bc35789b109d11ab14 [formerly edc1292d80b4e95bba725b8f7b0e5a0b6d2e4314]
    
    Former-commit-id: bb971caf538e987c39048b2d6a20c8b37c0e708f

--- bigdata/main.py
+++ bigdata/main.py
@@ -2,7 +2,7 @@ from sql_app import models
 from sql_app.database import engine
 import pandas as pd
 
-from recommend.users import recommend_user_by_tag_knn
+# from recommend.users import recommend_user_by_tag_knn
 
 # DB 테이블 생성
 models.Base.metadata.create_all(engine)
@@ -61,11 +61,62 @@ def data_into_bj_user():
 	df_bj_users.to_sql(name='bj_user', con=engine, if_exists='append', index=False)
 
 
+# solvedproblem 테이블 입력
+def data_into_solvedproblem():	
+	# solvedproblem 에 사용될 columns	
+	lately_solvedproblem_columns = ['userId', 'probNo', 'solvedDate']
+
+	# csv 파일 읽어오기 (데이터 프레임으로)
+	df_lately_solvedproblem = pd.read_csv(file_path + 'user_lately_solved_problems.csv')
+	# print(df_lately_solvedproblem)
+	# column 명 db에 맞게 변환
+	df_lately_solvedproblem.columns = lately_solvedproblem_columns	
+
+	print(df_lately_solvedproblem)
+
+	# db에 데이터 입력
+	df_lately_solvedproblem.to_sql(name='solvedproblem', con=engine, if_exists='append', index=False)
+
+# worngtype 테이블 입력
+def data_into_wrongtype():	
+
+	df_lately_worngproblem = pd.read_csv(file_path + 'wrong_pb.csv')
+	df_lately_worngproblem.loc[df_lately_worngproblem['result'].str.contains('런타임 에러'), 'result'] = '런타임 에러'
+	# worngtype = ['handle', '출력 형식이 잘못되었습니다', '틀렸습니다', '시간 초과', '메모리 초과', '출력 초과', '런타임 에러', '컴파일 에러']
+	worngtype_columns = ['no', 'userId', 'problems', 'typeName', 'timestamp']
+	df_lately_worngproblem.columns = worngtype_columns
+	print(df_lately_worngproblem)
+	series = df_lately_worngproblem.groupby('userId')['typeName'].value_counts()
+	df_wrongtype = series.reset_index(name='wrongCnt')
+	print(df_wrongtype)
+	df_wrongtype.to_sql(name='wrongtype', con=engine, if_exists='append', index=False)
+	# # wrongtype 에 사용될 columns	
+	# wrongtype_columns = ['typeName', 'userId', 'wrongCnt']
+
+	# # csv 파일 읽어오기 (데이터 프레임으로)
+	# df_lately_solvedproblem = pd.read_csv(file_path + 'user_lately_solved_problems.csv')
+	# # print(df_lately_solvedproblem)
+	# # column 명 db에 맞게 변환
+	# df_lately_solvedproblem.columns = lately_solvedproblem_columns	
+
+	# print(df_lately_solvedproblem)
+
+	# # db에 데이터 입력
+	# df_lately_solvedproblem.to_sql(name='solvedproblem', con=engine, if_exists='append', index=False)
+
+
 # db 연결
 conn = engine.connect()
 
 # major_category 테이블 입력
 # data_into_major_category()
-data_into_bj_user()
+
+# bj_user 테이블 입력
+# data_into_bj_user()
+
+# solvedproblem 테이블 입력
+# data_into_solvedproblem()
+
+data_into_wrongtype()
 
 conn.close()

88e21ea9dbae95b91a6ad8ebd8e8d6ab80f87975
Jin-Yuna <dbsdk100@gmail.com>
2022-10-03 20:33:54 +0900

    Fix : fix server address for merge
    Former-commit-id: eb070913516a77eb23cc536a3436d732438c1b39 [formerly eb070913516a77eb23cc536a3436d732438c1b39 [formerly ebdedfd9d8f573c00049968c8e9275af8a94cb33]]
    Former-commit-id: 7d74b9947e9fef47cfc505bb276a6840296b7eed [formerly 31c792e355becee7ab7c986a47d479c078f62933]
    
    Former-commit-id: b8b8efee483536de4b3124f82bbf83b3fadbe8b6

--- front/sharkshark/src/api/common.ts
+++ front/sharkshark/src/api/common.ts
@@ -1,7 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
 
-// const SERVER_ADDRESS = 'https://j7b205.p.ssafy.io/api';
-const SERVER_ADDRESS = 'http://127.0.0.1:8000';
+const SERVER_ADDRESS = 'https://j7b205.p.ssafy.io/api';
 
 export const authAxios: AxiosInstance = axios.create({
   baseURL: `${SERVER_ADDRESS}`,

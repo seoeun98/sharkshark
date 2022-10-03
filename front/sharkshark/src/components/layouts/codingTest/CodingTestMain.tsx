b93f73ca32be6725142f9ac51fcfb552bed84454
Jin-Yuna <dbsdk100@gmail.com>
2022-10-03 14:34:45 +0900

    Fix : fix timer
    Former-commit-id: f1da345e30d47aee85d6f3b489ab06a18cf44b26

--- front/sharkshark/src/components/layouts/codingTest/CodingTestMain.tsx
+++ front/sharkshark/src/components/layouts/codingTest/CodingTestMain.tsx
@@ -13,14 +13,25 @@ import {
   useDisclosure,
   HStack,
 } from '@chakra-ui/react';
-import { Key } from 'react';
+import { Key, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { CTproblem } from '../../../types/DataTypes';
 import ProblemItem from './Item/ProblemItem';
-import Timer from './Item/Timer';
+// import Timer from './Item/Timer';
 import { setCompStatus, setSolvingStatus, setStarttime } from '../../../reducers/CTReducer';
 import { ColorText } from '../../common/ColorText';
 import { Link } from 'react-router-dom';
+import Wave from 'react-wavify';
+import styled from '@emotion/styled';
+
+const WaveContainer = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  margin-top: 0vh;
+  border-radius: 10px;
+  overflow: hidden;
+`;
 
 const CodingTestMain = () => {
   const dispatch = useDispatch();
@@ -28,13 +39,28 @@ const CodingTestMain = () => {
   const CTstatus = useSelector((state: any) => state.CTReducer.solvingStatus);
   const allsolved = useSelector((state: any) => state.CTReducer.allsolved);
   const solvedNum = useSelector((state: any) => state.CTReducer.solvedNum);
+  const CTtimer = useSelector((state: any) => state.CTReducer.CTtimer);
+  const compStatus = useSelector((state: any) => state.CTReducer.compStatus);
+
+  const [hour, setHour] = useState(Math.floor(CTtimer / 60));
+  const [minute, setMinute] = useState(CTtimer - Math.floor(CTtimer / 60) * 60);
+
+  if (hour === 0 && minute === 0 && compStatus === 1) {
+    setHour(Math.floor(CTtimer / 60));
+    setMinute(CTtimer - Math.floor(CTtimer / 60) * 60);
+  }
+  const [hours, setHours] = useState(hour);
+  const [minutes, setMinutes] = useState(minute);
+  const [seconds, setSeconds] = useState(0);
+  const [remainTime, setremainTime] = useState(CTtimer);
+
   let today = new Date();
   let year = today.getFullYear(); // 년도
   let month = today.getMonth() + 1; // 월
   let date = today.getDate(); // 날짜
-  let hours = today.getHours(); // 시
-  let minutes = today.getMinutes(); // 분
-  let seconds = today.getSeconds(); // 초
+  let hhours = today.getHours(); // 시
+  let mminutes = today.getMinutes(); // 분
+  let sseconds = today.getSeconds(); // 초
 
   // assets & style
   const sharkjoonImage = useColorModeValue(
@@ -43,12 +69,13 @@ const CodingTestMain = () => {
   );
   const bgcolor = useColorModeValue('neutral.25', 'neutral.500');
   const modalBg = useColorModeValue('neutral.0', 'neutral.500');
+  const bg2 = useColorModeValue('neutral.0', 'neutral.800');
   const bg = useColorModeValue('white', 'neutral.800');
   const fontWeight = useColorModeValue(600, 400);
 
   const startCodeTEST = () => {
     dispatch(setSolvingStatus('start'));
-    dispatch(setStarttime(`${year}-${month}-${date} ${hours}:${minutes}-${seconds}`));
+    dispatch(setStarttime(`${year}-${month}-${date} ${hhours}:${mminutes}-${sseconds}`));
   };
 
   const { isOpen, onClose, onOpen } = useDisclosure();
@@ -77,18 +104,52 @@ const CodingTestMain = () => {
     onClose();
   };
 
+  useEffect(() => {
+    if (CTstatus === 'start') {
+      const countdown = setInterval(() => {
+        if (seconds > 0) {
+          setSeconds(seconds - 1);
+        }
+        if (seconds === 0) {
+          if (minutes === 0) {
+            clearInterval(countdown);
+            if (hours === 0) {
+              clearInterval(countdown);
+            } else {
+              setHours(hours - 1);
+              setMinutes(59);
+            }
+          } else {
+            setMinutes(minutes - 1);
+            setSeconds(59);
+          }
+        }
+        setremainTime(hours * 60 + minutes);
+      }, 1000);
+      return () => clearInterval(countdown);
+    } else {
+      setHours(hour);
+      setMinutes(minute);
+      setSeconds(0);
+    }
+  }, [CTstatus, CTtimer, hours, minutes, remainTime, seconds]);
+
+  if (CTstatus === 'start' && hours === 0 && minutes === 0 && seconds === 0) {
+    dispatch(setSolvingStatus('end'));
+  }
+
+  if (CTstatus === 'start' && hours === 0 && minutes === 0 && seconds === 0) {
+    dispatch(setSolvingStatus('end'));
+  }
+
+  let remainHeight = (CTtimer - remainTime) * (160 / CTtimer);
+
   return (
     <Center>
       <VStack w="50vw" spacing={10}>
         {' '}
         <Box borderRadius="10px">
-          <Center
-            bg={useColorModeValue('neutral.0', 'neutral.800')}
-            w="50vw"
-            h="4vh"
-            shadow="lg"
-            borderTopRadius="10px"
-          >
+          <Center bg={bg2} w="50vw" h="4vh" shadow="lg" borderTopRadius="10px">
             <Image src={sharkjoonImage} w="240px" />
           </Center>
           <Center
@@ -115,7 +176,80 @@ const CodingTestMain = () => {
             </Center>
           )}
         </VStack>
-        <Timer />
+        {/* 타이머 */}
+        <VStack
+          w="32vw"
+          h="16vh"
+          bg={bgcolor}
+          bgGradient={CTstatus === '' ? 'linear(to-r, #1B3E44, #2C2947)' : ''}
+          borderRadius="10px"
+          position="relative"
+        >
+          {CTstatus === '' ? (
+            <Box m="-0.5" />
+          ) : (
+            <WaveContainer>
+              <Wave
+                fill="url(#gradient)"
+                paused={false}
+                opacity="0.30"
+                options={{
+                  height: remainHeight,
+                  amplitude: 20,
+                  speed: 0.4,
+                  points: 6,
+                }}
+              >
+                <defs>
+                  <linearGradient id="gradient">
+                    <stop offset="10%" stopColor="rgba(153, 123, 237, 0.8)" />
+                    <stop offset="90%" stopColor="rgba(157, 236, 249, 0.8)" />
+                  </linearGradient>
+                </defs>
+              </Wave>
+            </WaveContainer>
+          )}
+
+          <VStack spacing={6}>
+            <Center
+              borderRadius="4px"
+              w="140px"
+              h="40px"
+              bgGradient="linear(to-r, primary.cyan50, primary.purple0)"
+              color="white"
+              fontWeight="600"
+              mt="-3vh"
+            >
+              {CTstatus === '' ? '제한 시간' : '남은 시간'}
+            </Center>
+            <HStack spacing={5}>
+              <Center w="72px" h="80px" bg="white" borderRadius="10px">
+                <VStack fontSize="36px" fontWeight="800" spacing={0}>
+                  <ColorText> {hours < 10 ? `0${hours}` : hours}</ColorText>
+                  <Box color="primary.purple0" fontWeight="700" fontSize="12px">
+                    HOURS
+                  </Box>
+                </VStack>
+              </Center>
+              <Center w="72px" h="80px" bg="white" borderRadius="10px">
+                <VStack fontSize="36px" fontWeight="800" spacing={0}>
+                  <ColorText> {minutes < 10 ? `0${minutes}` : minutes}</ColorText>
+                  <Box color="primary.purple0" fontWeight="700" fontSize="12px">
+                    MINUTES
+                  </Box>
+                </VStack>
+              </Center>
+              <Center w="72px" h="80px" bg="white" borderRadius="10px">
+                <VStack fontSize="36px" fontWeight="800" spacing={0}>
+                  <ColorText>{seconds < 10 ? `0${seconds}` : seconds}</ColorText>
+                  <Box color="primary.purple0" fontWeight="700" fontSize="12px">
+                    SECONDS
+                  </Box>
+                </VStack>
+              </Center>
+            </HStack>
+          </VStack>
+        </VStack>
         {CTstatus === 'start' ? (
           <Button size="cxl" onClick={closeCodeTest}>
             종료

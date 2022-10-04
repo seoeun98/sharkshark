023917783cfa4c1928cdd0c3755e5a0b26489295
Nelfunction <oat641@gmail.com>
2022-10-04 15:14:46 +0900

    Feat : save changes
    Former-commit-id: 82aa0f3a0f2c3c1e4a8e310fddc72b7faa73fec5

--- front/sharkshark/src/components/layouts/dataChart/DataRoadPage.tsx
+++ front/sharkshark/src/components/layouts/dataChart/DataRoadPage.tsx
@@ -1,3 +1,65 @@
+import { Box, Flex, Spacer, Tag } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { getRoadMapDataAPI } from '../../../api/auth/dataAnalysis';
+import { getRivalInfoAPI } from '../../../api/auth/rival';
+import { getUserID } from '../../../api/common';
+import { rival } from '../../../types/DataTypes';
+import { ColorText } from '../../common/ColorText';
+import { Tier } from '../../common/Tier';
+
 export const DataRoadPage = () => {
-  return <div>DataRoadPage</div>;
+  const [myInfo, setMyInfo] = useState<rival>({
+    tier: '0',
+    userId: '',
+    userClass: 0,
+    exp: 0,
+  });
+
+  const getUserInfo = async () => {
+    setMyInfo(await getRivalInfoAPI(getUserID()));
+    const RoadMapData = await getRoadMapDataAPI();
+    console.log(RoadMapData);
+  };
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
+
+  return (
+    <>
+      <Flex>
+        <Spacer />
+        <Box
+          fontSize="28px"
+          text-stroke="4px transparent"
+          bgGradient="linear(to-r, primary.cyan50, primary.purple0)"
+          color="Background"
+          style={{
+            WebkitBackgroundClip: 'text',
+            WebkitTextStroke: '2px transparent',
+          }}
+        >
+          {myInfo.userId}
+        </Box>
+        <Tag borderRadius="32px" ml="16px" px="16px">
+          <Tier level={parseInt(myInfo?.tier)} size="24px" />
+          <ColorText>&nbsp;Class {myInfo?.userClass}</ColorText>
+        </Tag>
+        <Spacer />
+      </Flex>
+      <Box pos="relative" p="32px" my="32px">
+        <Box
+          pos="absolute"
+          w="full"
+          h="full"
+          bgGradient="linear(to-br, #4AE2DE, #77A9E7, #997BED)"
+          borderRadius="12px"
+          filter="auto"
+          brightness="50%"
+          zIndex={-1}
+        />
+        <Box>ASS</Box>
+      </Box>
+    </>
+  );
 };

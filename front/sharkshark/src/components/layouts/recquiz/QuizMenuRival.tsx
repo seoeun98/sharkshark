7ca639dea41008fb47e02ea59ad4e724d30fb85b
Nelfunction <oat641@gmail.com>
2022-10-06 09:57:34 +0900

    Fix : Paragtaph phrase on RecProbs page
    Former-commit-id: 1d21a420562ba5fc5d42bf51dbe89d3cb89ad7c0

--- front/sharkshark/src/components/layouts/recquiz/QuizMenuRival.tsx
+++ front/sharkshark/src/components/layouts/recquiz/QuizMenuRival.tsx
@@ -2,7 +2,10 @@ import { Box, Button, Flex } from '@chakra-ui/react';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { probsByCategoryAPI } from '../../../api/auth';
+import { getUserID } from '../../../api/common';
 import { Problem } from '../../../types/DataTypes';
+import { ColorText } from '../../common/ColorText';
+import { Paragraph } from '../../common/Paragraph';
 import { QuizTable } from './QuizTable';
 import { QuizTableItem } from './QuizTableItem';
 
@@ -18,18 +21,25 @@ export const QuizMenuRival = () => {
   }, []);
 
   return (
-    <Box pos="relative">
-      <Link to="/recommend/user">
-        <Button pos="absolute" right="0">
-          라이벌 추가 등록
-        </Button>
-      </Link>
-      <Box fontSize="24px">라이벌 기반 추천 문제</Box>
-      <Box fontSize="12px">
-        지피지기(知彼知己)면 백전불태(百戰不殆) <br />
-        라이벌이 풀었지만, 내가 풀지 않았던 문제를 알려드려요.
-      </Box>
+    <Paragraph
+      title={
+        <Box pos="relative">
+          <Link to="/recommend/user">
+            <Button pos="absolute" right="0">
+              라이벌 추가 등록
+            </Button>
+          </Link>
+          <ColorText>{getUserID()}</ColorText> 님을 위한 추천 문제
+        </Box>
+      }
+      description={
+        <>
+          지피지기(知彼知己)면 백전불태(百戰不殆) <br />
+          라이벌이 풀었지만, 내가 풀지 않았던 문제를 알려드려요.
+        </>
+      }
+    >
       <QuizTable list={list} />
-    </Box>
+    </Paragraph>
   );
 };

5caf4893cb0af8d952a08d0b5adacde04c5d586a
Jin-Yuna <dbsdk100@gmail.com>
2022-10-06 10:20:26 +0900

    Fix : fix code test analysis
    Former-commit-id: 94e693bf2cbfcd657e70ab1f718855d7c10d0af4 [formerly ccaa7d6c704b87d06988b6d3e3e75508dfb6d1b6] [formerly ccaa7d6c704b87d06988b6d3e3e75508dfb6d1b6 [formerly d1ce895a2a9d3162085b5ba45a890e022b45572e]]
    Former-commit-id: e0defc33d6b69149547206b1acd07c71d7c1bae7 [formerly bdf4fc4dbd7add342da0640dd00e2f1d13f7095f]
    
    Former-commit-id: 0af0db2fd8c2102e55e08d5345479f9515697f32

--- front/sharkshark/src/views/LandingPage.tsx
+++ front/sharkshark/src/views/LandingPage.tsx
@@ -1,4 +1,5 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Center, Text } from '@chakra-ui/react';
+import { ColorText } from '../components/common/ColorText';
 import Footer from '../components/common/Footer';
 import FooterWave from '../components/common/FooterWave';
 
@@ -11,6 +12,12 @@ export const LandingPage = () => {
         LandingPage
       </Box>
 
+      {/* <Center h="100%">
+        <ColorText>샥샥!</ColorText>
+        <Box>막힘 없는</Box>
+        <Box>알고리즘 문제 풀이를 위한</Box>
+        <Box>문제 추천 서비스</Box>
+      </Center> */}
       <Footer />
       <Box w="100%" position="absolute" bottom={0}>
         <FooterWave />

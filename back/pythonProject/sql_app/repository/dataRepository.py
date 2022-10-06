4a826b432f5bfff50c0748eca97e72c0a1709c0c
최진호 <84559471+turtlebooster@users.noreply.github.com>
2022-10-06 11:18:37 +0900

    Fix : use round for users avg datas
    Former-commit-id: 2aa250923c15f5b77976abedbb9581bdba0155c5 [formerly 6d49fb74b63bf91c0fd2a6ee99c86ee6586d0c53] [formerly 6d49fb74b63bf91c0fd2a6ee99c86ee6586d0c53 [formerly bf04d309f4cdcb4acba94012c6a36f6aaa07b618]]
    Former-commit-id: d5a94621e79b375b3086534eb00ca085ce604cf7 [formerly 38b061750d3511ff47b8630ac052dee6d6a85023]
    
    Former-commit-id: ff9e7fb91c2a12eb75e812baa594077b781f0695

--- back/pythonProject/sql_app/repository/dataRepository.py
+++ back/pythonProject/sql_app/repository/dataRepository.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 import numpy as np
+import math
 
 from requests import Session
 from sql_app.repository import rivalRepository
@@ -102,7 +103,7 @@ def get_level_avg(userId: str, db: Session):
     for one in pb_list:
         pb_lv_list.append(one.level)    
 
-    lv_avg = np.mean(pb_lv_list)
+    lv_avg = round(np.mean(pb_lv_list), 2)
 
     return pb_list, lv_avg
 
@@ -117,6 +118,6 @@ def get_pb_per_week(userId: str, db: Session):
         rival_probs = db.query(models.solvedProblem).filter(models.solvedProblem.userId == one).order_by(models.solvedProblem.solvedDate).all()
         recent_date = datetime.strptime(str(rival_probs[-1].solvedDate), "%Y-%m-%d %H:%M:%S").date()
         last_date = datetime.strptime(str(rival_probs[0].solvedDate), "%Y-%m-%d %H:%M:%S").date() 
-        res_list.append({"userId" : one, "pb_per_week" : 7 * len(rival_probs) / (abs((recent_date - last_date).days) + 1)})
+        res_list.append({"userId" : one, "pb_per_week" : round(7 * len(rival_probs) / (abs((recent_date - last_date).days) + 1), 2)})
 
     return res_list
